b2f6ab9fb6f6a1a4cf0fd642ee7c8d2c0ad7ee0c
Felipe Bertanha <bertanhahp@hotmail.com>
2018-08-22 19:00:16 -0300

    Aula 3

---
+++ app/src/main/java/bertanha/com/br/aluraviagens/ui/activity/PagamentoActivity.java
@@ -0,0 +1,15 @@
+package bertanha.com.br.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bertanha.com.br.aluraviagens.R;
+
+public class PagamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
